f01461f31e21c11106ec58b66cc08a45c1733519
Pamela-WMachado <pamela.wehle@gmail.com>
2022-06-02 14:31:43 -0300

    config da criptografia do token pt1

--- src/main/java/com/aulas/rest/AppConfig.java
+++ src/main/java/com/aulas/rest/AppConfig.java
@@ -1,5 +1,27 @@
 package com.aulas.rest;
 
-public class AppConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+@Configuration
+public class AppConfig {
+	@Bean 
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean 
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey("JwtSecret");
+		return tokenConverter;
+	}
+	
+	@Bean 
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
 }
